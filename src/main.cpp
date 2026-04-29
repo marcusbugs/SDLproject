0669bcabfd030ed72bff0b0ef8eb469b92a0a609
cassel2 <cassel2@stolaf.edu>
2026-04-29 14:35:55 -0500

    aprildog

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,70 @@
 //
 // Created by MJC11 on 4/29/2026.
+// Surface-blitting version (no SDL_Renderer)
 //
 #include <SDL2/SDL.h>
-#include <cstdio>
 #include <iostream>
-//using namespace std;
 
-int main(int argc, char* argv[]) { //sdl wants args it runs some macro on it on start up
-
-    if (SDL_Init(SDL_INIT_VIDEO)<0) {
-        std::cerr << SDL_GetError() << std::endl;
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
         return 1;
-    } //set up video stuff
+    }
 
-    SDL_Window* window = SDL_CreateWindow("goblin", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-    if (window == NULL) {
-        std::cerr << SDL_GetError() << std::endl;
+    SDL_Window* window = SDL_CreateWindow("goblin",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,800, 600,SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    if (!renderer) {
-        std::cerr << "SDL_CreateRenderer failed: " << SDL_GetError() << std::endl;
+    SDL_Surface* windowSurface = SDL_GetWindowSurface(window);
+    if (!windowSurface) {
+        std::cerr << "SDL_GetWindowSurface failed: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
         SDL_Quit();
         return 1;
     }
-    SDL_Rect square = {350, 250, 100, 100};
-    bool running = true;
-    SDL_Rect square2 = {350, 250, 300, 200};
+
+    SDL_Surface* appledog = SDL_LoadBMP("bogos/appledog.bmp");
+    if (!appledog) {
+        std::cerr << "SDL_LoadBMP failed: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    Uint32 green = SDL_MapRGB(windowSurface->format, 0, 255, 0);
+
+    SDL_Rect square = {350, 250, 200, 200};
 
     const Uint8* keys = SDL_GetKeyboardState(NULL);
 
+    bool running = true;
     while (running) {
+
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = false;
-            }
+            if (event.type == SDL_QUIT) running = false;
         }
 
-        if (keys[SDL_SCANCODE_D]) square2.x++;
-        if (keys[SDL_SCANCODE_A]) square2.x--;
-        if (keys[SDL_SCANCODE_W]) square2.y--;
-        if (keys[SDL_SCANCODE_S]) square2.y++;
-        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-        SDL_RenderClear(renderer);
+        if (keys[SDL_SCANCODE_D]) square.x++;
+        if (keys[SDL_SCANCODE_A]) square.x--;
+        if (keys[SDL_SCANCODE_W]) square.y--;
+        if (keys[SDL_SCANCODE_S]) square.y++;
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);  // white
-        SDL_RenderFillRect(renderer, &square);
 
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);  // white
-        SDL_RenderFillRect(renderer, &square2);
+        SDL_FillRect(windowSurface, NULL, green);
 
-        SDL_RenderPresent(renderer);
+        SDL_Rect dst = square;
 
+        SDL_BlitSurface(appledog, NULL, windowSurface, &dst);
+
+        SDL_UpdateWindowSurface(window);
     }
 
-    SDL_DestroyRenderer(renderer);
+    SDL_FreeSurface(appledog);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
     return 0;
 }
