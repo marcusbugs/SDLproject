9e4483fe28abb58fcb6d81c4a62fe55904a362a4
cassel2 <cassel2@stolaf.edu>
2026-04-30 09:18:36 -0500

    2d vector

--- src/Vec2.hpp
+++ src/Vec2.hpp
@@ -4,10 +4,46 @@
 
 #ifndef SDLPROJECT_VEC2_HPP
 #define SDLPROJECT_VEC2_HPP
+#include <cmath>
+
 struct Vec2 {
     float x, y;
     Vec2() : x(0), y(0) {}
     Vec2(float x, float y) : x(x), y(y) {}
-
-}
+    Vec2 operator+(const Vec2& rvec) const {
+        return Vec2(x + rvec.x, y + rvec.y);
+    }
+    Vec2& operator+=(const Vec2& rvec) {
+        x += rvec.x;
+        y += rvec.y;
+        return *this;
+    }
+    Vec2& operator-=(const Vec2& rvec) {
+        x -= rvec.x;
+        y -= rvec.y;
+        return *this;
+    }
+    Vec2 operator-(const Vec2& rvec) const {
+        return Vec2(x - rvec.x, y - rvec.y);
+    }
+    Vec2 operator*(float scalar) const {
+        return Vec2(x * scalar, y * scalar);
+    }
+    Vec2 operator/(float scalar) const {
+        return Vec2(x / scalar, y / scalar);
+    }
+    float length() const{
+        return sqrt(x * x + y * y);
+    }
+    float lengthSquared() const {
+        return x * x + y * y;
+    }
+    bool normalize() {
+        float l = length();
+        if (l < 0.0001f) return false;
+        x /= l;
+        y /= l;
+        return true;
+    }
+};
 #endif //SDLPROJECT_VEC2_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
+#include <iostream>
+#include <ostream>
+
+#include "Color.hpp"
 #include "Application.hpp"
+#include "Vec2.hpp"
 
 int main(int argc, char* argv[]) {
+    Vec2 a(10, 20);
+    Vec2 b(20, -20);
+    std::cout << a.length() << std::endl;
+    std::cout << b.length() << std::endl;
+    Vec2 c = a + b;
+    std::cout << c.length() << std::endl;
+
     Application app;
 
     if (!app.Init(800, 800)) return 1;
